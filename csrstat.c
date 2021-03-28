0f4093230788b628e8a3b764f474bcfa51d23477
Evgeny Istratov <warexify@outlook.com>
2021-03-28 13:13:58 +0700

    Updating tool to v2.0
    -added full flags to output
    -added csrutil arguments to output
    -added CSR_ALLOW_EXECUTABLE_POLICY_OVERRIDE (xnu-4903.221.2)

--- csrstat.c
+++ csrstat.c
@@ -2,6 +2,8 @@
  * Created: 23 August 2015
  * Name...: csrstat.c
  * Author.: Pike R. Alpha
+ * Edited.: 10 November 2018
+ * Author.: Joss Brown
  * Purpose: Command line tool for El Capitan and greater to get the active SIP status.
  *
  * Compile with: cc csrstat.c -o csrstat
@@ -16,6 +18,11 @@
  *			- renamed CSR_ALLOW_USER_APPROVED_KEXTS to CSR_ALLOW_UNAPPROVED_KEXTS.
  *			- flip output of 'Device Configuration'.
  *			- changed the text of the CSR_ALLOW_UNAPPROVED_KEXTS setting.
+ *			-added full flags to output
+ *			-added csrutil arguments to output
+ *			-added CSR_ALLOW_EXECUTABLE_POLICY_OVERRIDE (xnu-4903.221.2)
+ *
+ * See also: https://github.com/opensource-apple/xnu/blob/master/bsd/sys/csr.h
  */
 
 #include <time.h>
@@ -28,7 +35,7 @@
 typedef uint32_t csr_config_t;
 
 char *text = NULL;
-double gVersion = 1.8;
+double gVersion = 2.0;
 csr_config_t config = 0;
 
 /* Rootless configuration flags */
@@ -42,6 +49,7 @@ csr_config_t config = 0;
 #define CSR_ALLOW_DEVICE_CONFIGURATION	(1 << 7)	// 128
 #define CSR_ALLOW_ANY_RECOVERY_OS		(1 << 8)	// 256
 #define CSR_ALLOW_UNAPPROVED_KEXTS		(1 << 9)	// 512
+#define CSR_ALLOW_EXECUTABLE_POLICY_OVERRIDE	(1 << 10)	// 1024
 
 #define CSR_VALID_FLAGS (CSR_ALLOW_UNTRUSTED_KEXTS | \
 	CSR_ALLOW_UNRESTRICTED_FS | \
@@ -52,7 +60,8 @@ csr_config_t config = 0;
 	CSR_ALLOW_UNRESTRICTED_NVRAM  | \
 	CSR_ALLOW_DEVICE_CONFIGURATION | \
 	CSR_ALLOW_ANY_RECOVERY_OS | \
-	CSR_ALLOW_UNAPPROVED_KEXTS)
+	CSR_ALLOW_UNAPPROVED_KEXTS | \
+	CSR_ALLOW_EXECUTABLE_POLICY_OVERRIDE)
 
 /* Syscalls */
 extern int csr_get_active_config(csr_config_t *config);
@@ -101,7 +110,7 @@ int main(int argc, const char * argv[])
 	// Syscall
 	csr_get_active_config(&config);
 	
-	printf("csrstat v%.1f Copyright (c) 2015-%d by Pike R. Alpha\n", gVersion, (tm.tm_year + 1900));
+	printf("csrstat v%.1f Copyright (c) 2015-2017 by Pike R. Alpha, 2017-%d by Joss Brown\n", gVersion, (tm.tm_year + 1900));
 	//
 	// Note: boot.efi is no longer using 0x67 but 0x77 for csrutil disabled!!!
 	//
@@ -119,19 +128,22 @@ int main(int argc, const char * argv[])
 		}
 	}
 	
-	printf("\n\nConfiguration:\n");
 
-	printf("\tApple Internal...........: %s\n", _csr_check(CSR_ALLOW_APPLE_INTERNAL, 0));
-	printf("\tKext Signing Restrictions: %s\n", _csr_check(CSR_ALLOW_UNTRUSTED_KEXTS, 1));
-	printf("\tTask for PID Restrictions: %s\n", _csr_check(CSR_ALLOW_TASK_FOR_PID, 1));
-	printf("\tFilesystem Protections...: %s\n", _csr_check(CSR_ALLOW_UNRESTRICTED_FS, 1));
-	printf("\tDebugging Restrictions...: %s\n", _csr_check(CSR_ALLOW_KERNEL_DEBUGGER, 1));
-	printf("\tDTrace Restrictions......: %s\n", _csr_check(CSR_ALLOW_UNRESTRICTED_DTRACE, 1));
-	printf("\tNVRAM Protections........: %s\n", _csr_check(CSR_ALLOW_UNRESTRICTED_NVRAM, 1));
-	printf("\tDevice Configuration.....: %s\n", _csr_check(CSR_ALLOW_DEVICE_CONFIGURATION, 0));
-	printf("\tBaseSystem Verification..: %s\n", _csr_check(CSR_ALLOW_ANY_RECOVERY_OS, 1));
-	printf("\tAllow Unapproved Kexts...: %s\n", _csr_check(CSR_ALLOW_UNAPPROVED_KEXTS, 1));
-	
+	printf("\n\nCurrent Configuration:\n");
+	printf("\tApple Internal\t\t\t%s\t[--no-internal]\t\tCSR_ALLOW_APPLE_INTERNAL\n", _csr_check(CSR_ALLOW_APPLE_INTERNAL, 0));
+	printf("\tKext Signing\t\t\t%s\t[--without kext]\tCSR_ALLOW_UNTRUSTED_KEXTS\n", _csr_check(CSR_ALLOW_UNTRUSTED_KEXTS, 1));
+	printf("\tDebugging Restrictions\t\t%s\t[--without debug]\tCSR_ALLOW_TASK_FOR_PID\n", _csr_check(CSR_ALLOW_TASK_FOR_PID, 1));
+	printf("\tFilesystem Protections\t\t%s\t[--without fs]\t\tCSR_ALLOW_UNRESTRICTED_FS\n", _csr_check(CSR_ALLOW_UNRESTRICTED_FS, 1));
+	printf("\tKernel Debugging Restrictions\t%s\t<n/a>\t\t\tCSR_ALLOW_KERNEL_DEBUGGER\n", _csr_check(CSR_ALLOW_KERNEL_DEBUGGER, 1));
+	printf("\tDTrace Restrictions\t\t%s\t[--without dtrace]\tCSR_ALLOW_UNRESTRICTED_DTRACE\n", _csr_check(CSR_ALLOW_UNRESTRICTED_DTRACE, 1));
+	printf("\tNVRAM Protections\t\t%s\t[--without nvram]\tCSR_ALLOW_UNRESTRICTED_NVRAM\n", _csr_check(CSR_ALLOW_UNRESTRICTED_NVRAM, 1));
+	printf("\tDevice Configuration\t\t%s\t<n/a>\t\t\tCSR_ALLOW_DEVICE_CONFIGURATION\n", _csr_check(CSR_ALLOW_DEVICE_CONFIGURATION, 0));
+	printf("\tBaseSystem Verification\t\t%s\t[--without basesystem]\tCSR_ALLOW_ANY_RECOVERY_OS\n", _csr_check(CSR_ALLOW_ANY_RECOVERY_OS, 1));
+	printf("\tUnapproved Kexts Restrictions\t%s\t<n/a>\t\t\tCSR_ALLOW_UNAPPROVED_KEXTS\n", _csr_check(CSR_ALLOW_UNAPPROVED_KEXTS, 1));
+	printf("\tExecutable Policy\t\t%s\t<n/a>\t\t\tCSR_ALLOW_EXECUTABLE_POLICY_OVERRIDE\n", _csr_check(CSR_ALLOW_EXECUTABLE_POLICY_OVERRIDE, 1));
+	printf("\nBoot into Recovery Mode and modify with: 'csrutil enable [arguments]'\n");
+	printf("<Note: some flags are not accessible using the csrutil CLI.>\n");
+
 	if (config && (config != CSR_ALLOW_APPLE_INTERNAL))
 	{
 		printf("\nThis is an unsupported configuration, likely to break in the future and leave your machine in an unknown state.\n");
